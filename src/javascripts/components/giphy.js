06e0fedc3cdf21503297a564092c0c77b0cbb274
Sam Mudick <smudick@gmail.com>
2020-10-03 17:10:59 -0500

    removed gifs

--- src/javascripts/components/giphy.js
+++ src/javascripts/components/giphy.js
@@ -1,38 +1,38 @@
-import moment from 'moment';
-import datas from '../helpers/data/messageData';
-import display from './displayMessages';
-import selectUser from './selectUser';
-import emoji from './emojis';
-import APIKEYS from './apiKeys';
+// import moment from 'moment';
+// import datas from '../helpers/data/messageData';
+// import display from './displayMessages';
+// import selectUser from './selectUser';
+// import emoji from './emojis';
+// import APIKEYS from './apiKeys';
 
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
-const giphyButton = () => {
-  $('#addGif').on('click', () => {
-    let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEYS.APIKEYS}&limit=1&q=`;
-    const str = $('#giphy').val().trim();
-    url = url.concat(str);
-    fetch(url)
-      .then((data) => data.json())
-      .then((content) => {
-        const gifLink = content.data[0].images.downsized.url;
-        const newId = datas.randomId();
-        const newMessage = {
-          id: newId,
-          user: selectUser.selectUser(),
-          time: moment().format('MMMM Do YYYY, h:mm a'),
-          text: emoji.emojis(),
-          gif: gifLink
-        };
-        datas.getMessages().push(newMessage);
-        $('#messageDisplay').html('');
-        display.displayMessages();
-        $('#giphy').val('');
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  });
-};
+// const giphyButton = () => {
+//   $('#addGif').on('click', () => {
+//     let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEYS.APIKEYS}&limit=1&q=`;
+//     const str = $('#giphy').val().trim();
+//     url = url.concat(str);
+//     fetch(url)
+//       .then((data) => data.json())
+//       .then((content) => {
+//         const gifLink = content.data[0].images.downsized.url;
+//         const newId = datas.randomId();
+//         const newMessage = {
+//           id: newId,
+//           user: selectUser.selectUser(),
+//           time: moment().format('MMMM Do YYYY, h:mm a'),
+//           text: emoji.emojis(),
+//           gif: gifLink
+//         };
+//         datas.getMessages().push(newMessage);
+//         $('#messageDisplay').html('');
+//         display.displayMessages();
+//         $('#giphy').val('');
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//       });
+//   });
+// };
 
-export default { giphyButton };
+// export default { giphyButton };
